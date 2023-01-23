1cba510e765db3acc2845cef19a029e8aaad6ef9
Praveen2604 <balapraveen2604@gmail.com>
2023-01-23 15:22:03 +0530

    delete learning code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { RoomsComponent } from './rooms/rooms.component';
-import { Room1Component } from './room1/room1.component';
 import { SearchfilterPipe } from './searchfilter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    RoomsComponent,
-    Room1Component,
     SearchfilterPipe
   ],
   imports: [
